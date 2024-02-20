fad4b30102c2b3e22df31c65e22b8368d55c827e
Varunryaliiith <ee23btech11220@iith.ac.in>
2024-02-20 18:52:55 +0530

    update

--- assignments/digital2/codes/digital2.py
+++ assignments/digital2/codes/digital2.py
@@ -15,7 +15,6 @@ highlight_point = points[highlight_index]
 
 # Plot stem graph
 plt.stem(n_values, points, basefmt='b-')
-plt.stem(highlight_n, highlight_point, linefmt='r-', markerfmt='ro', basefmt='r-')  # Highlight stem at n=19
 plt.xlabel('n')
 plt.ylabel('x(n)')
 plt.grid(True)
